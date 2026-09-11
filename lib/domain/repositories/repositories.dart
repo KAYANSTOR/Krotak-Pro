0e5a96a0eb464b6da68da9fd30ebe92d7ad6bad9
KAYANSTOR <jarallhalkboudi@gmail.com>
2026-09-11 23:36:49 +0300

    feat(ui-p0): real-data screens, routing, async states, repo list APIs

--- lib/domain/repositories/repositories.dart
+++ lib/domain/repositories/repositories.dart
@@ -1,55 +1,88 @@
+import '../../core/result.dart';
 import '../entities/audit.dart';
 import '../entities/card.dart';
 import '../entities/customer.dart';
+import '../entities/license.dart';
 import '../entities/message.dart';
 import '../entities/setting.dart';
 import '../entities/transaction.dart';
 import '../entities/wallet.dart';
-import '../../core/result.dart';
 
 abstract interface class CustomerRepository {
-  Future<Result<Customer>> getById(String id);
-  Future<Result<Customer?>> findByPhone(String phone);
-  Future<Result<List<Customer>>> list({int limit = 50, int offset = 0});
-  Future<Result<Customer>> save(Customer customer);
+  Future<Result<Customer?>> findById(String id);
+  Future<Result<Customer?>> findByIdentifier(String value);
+  Future<Result<List<Customer>>> search(String query);
+  Future<Result<List<CustomerIdentifier>>> listIdentifiers(String customerId);
+  Future<Result<void>> save(Customer customer);
+  Future<Result<void>> saveIdentifier(CustomerIdentifier identifier);
 }
 
 abstract interface class WalletRepository {
-  Future<Result<Wallet>> getById(String id);
-  Future<Result<Wallet?>> findByCustomerAndCurrency(String customerId, String currencyCode);
-  Future<Result<Wallet>> save(Wallet wallet);
+  Future<Result<Wallet?>> findById(String id);
+  Future<Result<List<Wallet>>> listAll();
+  Future<Result<void>> save(Wallet wallet);
+}
+
+abstract interface class PointOfSaleRepository {
+  Future<Result<PointOfSale?>> findById(String id);
+  Future<Result<List<PointOfSale>>> listAll();
+  Future<Result<void>> save(PointOfSale pointOfSale);
 }
 
 abstract interface class CardCategoryRepository {
-  Future<Result<CardCategory>> getById(String id);
-  Future<Result<List<CardCategory>>> listActive();
-  Future<Result<CardCategory>> save(CardCategory category);
+  Future<Result<CardCategory?>> findById(String id);
+  Future<Result<List<CardCategory>>> listAll();
+  Future<Result<void>> save(CardCategory category);
 }
 
-abstract interface class CardInventoryRepository {
-  Future<Result<Card>> getById(String id);
-  Future<Result<Card?>> findBySerial(String serial);
-  Future<Result<List<Card>>> listAvailable(String categoryId, {int limit = 20});
-  Future<Result<Card>> save(Card card);
-}
-
-abstract interface class MessageRepository {
-  Future<Result<Message>> getById(String id);
-  Future<Result<Message?>> findByDedupeKey(String dedupeKey);
-  Future<Result<Message>> save(Message message);
+abstract interface class CardRepository {
+  Future<Result<Card?>> findById(String id);
+  Future<Result<Card?>> findBySerialNumber(String serialNumber);
+  Future<Result<List<Card>>> findByCategory(String categoryId);
+  Future<Result<List<Card>>> findAvailableByCategory(String categoryId);
+  Future<Result<List<Card>>> listByStatus(CardStatus status);
+  Future<Result<void>> save(Card card);
+  Future<Result<int>> expireReservations(DateTime now);
+  Future<Result<void>> reserve(String cardId, CardReservation reservation);
+  Future<Result<void>> releaseReservation(String cardId, String reservationId);
+  Future<Result<void>> markSold(String cardId, String saleId);
+  Future<Result<void>> restoreAvailable(String cardId);
 }
 
 abstract interface class TransactionRepository {
-  Future<Result<Transaction>> getById(String id);
-  Future<Result<List<Transaction>>> listByCustomer(String customerId, {int limit = 50});
-  Future<Result<Transaction>> save(Transaction transaction);
+  Future<Result<void>> append(Transaction transaction);
+  Future<Result<List<Transaction>>> findByCustomer(String customerId);
+  Future<Result<Transaction?>> findByReference(String reference);
+  Future<Result<List<Transaction>>> listRecent({int limit = 50});
 }
 
-abstract interface class AuditRepository {
-  Future<Result<void>> append(AuditLog entry);
+abstract interface class SaleRepository {
+  Future<Result<void>> save(Sale sale);
+  Future<Result<Sale?>> findById(String id);
+  Future<Result<List<Sale>>> findByCustomer(String customerId);
+  Future<Result<List<Sale>>> listRecent({int limit = 50});
+  Future<Result<List<Sale>>> listCompletedBetween(DateTime from, DateTime to);
 }
 
-abstract interface class SettingRepository {
-  Future<Result<AppSetting?>> get(String key);
-  Future<Result<void>> set(AppSetting setting);
+abstract interface class MessageRepository {
+  Future<Result<void>> save(IncomingMessage message);
+  Future<Result<IncomingMessage?>> findById(String id);
+  Future<Result<IncomingMessage?>> findByExternalReference(String reference);
+  Future<Result<List<IncomingMessage>>> pendingProcessing();
+  Future<Result<void>> updateStatus(String id, MessageProcessingStatus status);
+}
+
+abstract interface class LicenseRepository {
+  Future<Result<License?>> getCurrent();
+  Future<Result<void>> save(License license);
+}
+
+abstract interface class SettingsRepository {
+  Future<Result<AppSetting?>> find(String key);
+  Future<Result<void>> save(AppSetting setting);
+}
+
+abstract interface class AuditLogRepository {
+  Future<Result<void>> append(AuditLog log);
+  Future<Result<List<AuditLog>>> findByEntity(String entityType, String entityId);
 }
