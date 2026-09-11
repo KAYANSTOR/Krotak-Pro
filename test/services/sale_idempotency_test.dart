11d9ee26a65082f0537f6dc9bf52e86e1409eb24
KAYANSTOR <jarallhalkboudi@gmail.com>
2026-09-12 02:42:46 +0300

    fix(test): import DriftUnitOfWork for sale idempotency test

--- test/services/sale_idempotency_test.dart
+++ test/services/sale_idempotency_test.dart
@@ -5,6 +5,7 @@ import 'package:net_app/core/id_generator.dart';
 import 'package:net_app/core/result.dart';
 import 'package:net_app/data/database/app_database.dart'
     hide Customer, Card, Sale, TransferTemplate, CardCategory, Transaction;
+import 'package:net_app/data/database/drift_unit_of_work.dart';
 import 'package:net_app/data/repositories/local_repositories.dart';
 import 'package:net_app/domain/entities/card.dart';
 import 'package:net_app/domain/entities/customer.dart';
