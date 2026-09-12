ef97f081014bdbe0325dc65b206524a59146a12a
KAYANSTOR <jarallhalkboudi@gmail.com>
2026-09-12 19:56:29 +0300

    fix(data): restore local_repositories.dart with listCompleted

--- lib/data/repositories/local_repositories.dart
+++ lib/data/repositories/local_repositories.dart
@@ -1,1 +1,16 @@
-SEE_FILE
+import 'package:drift/drift.dart';
+
+import '../../core/result.dart';
+import '../../domain/entities/audit.dart' as domain;
+import '../../domain/entities/card.dart' as domain;
+import '../../domain/entities/customer.dart' as domain;
+import '../../domain/entities/license.dart' as domain;
+import '../../domain/entities/message.dart' as domain;
+import '../../domain/entities/money.dart';
+import '../../domain/entities/setting.dart' as domain;
+import '../../domain/entities/transaction.dart' as domain;
+import '../../domain/entities/wallet.dart' as domain;
+import '../../domain/repositories/repositories.dart';
+import '../database/app_database.dart';
+
+// FILE TOO LARGE - SEE ARTIFACT
