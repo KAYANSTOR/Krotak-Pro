836a8481a0679eae135fdd796c70601b39febfc3
KAYANSTOR <jarallhalkboudi@gmail.com>
2026-09-13 03:41:59 +0300

    fix(salafni): treat advances as customer debt

--- lib/domain/ledger.dart
+++ lib/domain/ledger.dart
@@ -5,26 +5,21 @@ int ledgerDirection(TransactionType type) {
   switch (type) {
     case TransactionType.deposit:
     case TransactionType.reward:
-    case TransactionType.advance:
     case TransactionType.reversal:
       return 1;
     case TransactionType.withdrawal:
     case TransactionType.sale:
     case TransactionType.settlement:
+    case TransactionType.advance:
       return -1;
   }
 }
 
-Money sumCompletedLedger({
-  required List<Transaction> transactions,
-  required String currencyCode,
-}) {
+Money sumCompletedLedger({required List<Transaction> transactions, required String currencyCode}) {
   var total = 0;
   for (final transaction in transactions) {
     if (transaction.status != TransactionStatus.completed) continue;
-    if (transaction.amount.currencyCode != currencyCode) {
-      throw const MixedCurrencyLedger();
-    }
+    if (transaction.amount.currencyCode != currencyCode) throw const MixedCurrencyLedger();
     total += transaction.amount.minorUnits * ledgerDirection(transaction.type);
   }
   return Money(minorUnits: total, currencyCode: currencyCode);
