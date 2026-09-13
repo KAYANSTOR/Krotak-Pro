240bc45ddc7295e59b23b605a7b2c13e19b96ceb
KAYANSTOR <jarallhalkboudi@gmail.com>
2026-09-13 03:44:08 +0300

    fix(test): use package imports for salafni foundation test

--- test/services/salafni_foundation_test.dart
+++ test/services/salafni_foundation_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
-
-import '../../lib/domain/ledger.dart';
-import '../../lib/domain/entities/transaction.dart';
+import 'package:net_app/domain/entities/transaction.dart';
+import 'package:net_app/domain/ledger.dart';
 
 void main() {
   test('advance is a debt direction in the shared ledger', () {
