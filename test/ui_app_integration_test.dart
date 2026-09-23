09d63ad26fe8b44d0ce84a191fb5dc18ecb2b013
Jarallh AL-kaboudi <jarallhalkboudi@gmail.com>
2026-09-23 06:42:39 +0300

    fix(ci): hide Flutter Card import in ui_app_integration_test

--- test/ui_app_integration_test.dart
+++ test/ui_app_integration_test.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:drift/native.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Card;
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:net_app/application/app_container.dart';
